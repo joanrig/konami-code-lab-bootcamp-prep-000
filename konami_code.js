9d13b9074158bfa7bf72272fda5250744dca500a
Joan in DC <joanrigdon@gmail.com>
2019-01-16 22:54:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+  const body = document.body
+  
+  body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    let i = 0; i < codes.length;
+    
+    if (key === codes[i]) {
+       i++ ;
+       
+       if (i === codes.length) {
+          alert('Hurray!');
+          index = 0;
+       }
+    } else {
+      index = 0;
+    }
+  });
 }
